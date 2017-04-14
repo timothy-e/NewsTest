cfa54ffb3f72856b3f393f01dc2da4d6d0714e8a
Tim <timothy.elgersma@gmail.com>
2017-04-14 13:12:49 -0400

    AsyncTask now works, need to figure out how to read HTML

--- app/src/main/java/ca/tdchristian/newstest/News.java
+++ app/src/main/java/ca/tdchristian/newstest/News.java
@@ -1,5 +1,6 @@
 package ca.tdchristian.newstest;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,30 +8,71 @@ import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class News extends AppCompatActivity {
 
     TextView output;
-
-    protected Document getHTML(){
-        try {
-            return Jsoup.connect("http://tdchristian.ca/news").get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    Document doc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         output = (TextView)findViewById(R.id.textView);
-        Document doc = getHTML();
-        output.setText(doc.title());
+        try {
+            doc = new retrieveHTML().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Elements allElements = doc.select("b");
+        int size = allElements.size();
+        /*for (Element i : allElements) {
+            output.setText(i + " " + i.text());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            texts.concat("\n" + i.text());
+        }*/
+        output.setText(size);
+        //output.setText(doc.select("b").first().text());
+        /*Element content = doc.getElementById("PAGES_CONTAINERinlineContent");
+        Elements paragraphs = doc.getElementsByTag("h1");
+        String texts = "";
+        for (Element i : paragraphs){
+            texts.concat("\n" + i.text());
+        }
+        output.setText(texts);
+        if (texts.equals("")){
+            output.setText(doc.toString());
+        }*/
 
 
+    }
+}
+
+class retrieveHTML extends AsyncTask<Void, Void, Document> {
+    @Override
+    protected Document doInBackground(Void... Void){
+        try {
+            return Jsoup.connect("http://splash.tdchristian.ca/classes/math/Hagen/C&V/").get();
+            //return Jsoup.connect("http://tdchristian.ca/news").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.i("ERROR:", "IOException e, couldn't connect");
+        return null;
+    }
+    @Override
+    protected void onPostExecute(Document doc) {
+        super.onPostExecute(doc);
     }
 }
